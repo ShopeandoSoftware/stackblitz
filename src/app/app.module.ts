67891fc361a4073155b8c2fc238bf48377a2dfbc
ShopeandoSoftware <software@shopeando.mx>
2021-06-24 11:51:18 -0500

    1 file modified, 3 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './topbar/topbar.component';
+import { TopBarComponent } from './top-bar/topbar.component';
 
 @NgModule({
   imports: [BrowserModule, ReactiveFormsModule, RouterModule],
